8f4cadc91bd4f2c752a7dbf84cc7d7b2dae78e68
Val_1 S_z <csa.aiden@gmail.com>
2021-01-23 21:32:34 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,12 +1,17 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {}
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
